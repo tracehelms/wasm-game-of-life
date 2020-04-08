042316bbbfc26432b6b2e41f3f5dfcfc03460f73
Trace Helms <tracehelms@gmail.com>
2020-04-08 07:38:32 -0600

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,16 @@ impl Universe {
         self.cells.as_ptr()
     }
 
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..height * self.width).map(|_i| Cell::Dead).collect();
+    }
+
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
 
@@ -118,6 +128,19 @@ impl Universe {
             }
         }
         count
+    }
+}
+
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
     }
 }
 
